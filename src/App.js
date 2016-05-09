dfb651c9f09ba4775230f00650d5ca19028d75af
paul <paul.christophe6@gmail.com>
2016-05-09 19:38:57 -0400

    fav beans

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
+// packages can be imported just by their name
 import React, { Component } from 'react';
 
+// local files have must be imported with relative paths
+import BeanItem from './BeanItem';
+import data from './data';
+
+console.log(data);
 export default class App extends Component {
   render() {
     return (
-      <h1>Hello, world.</h1>
+      <div>
+        <h1>Fav Beans</h1>
+
+        {
+          data.map((bean, i) => {
+            return <BeanItem bean={bean} key={i} />
+          })
+        }
+      </div>
     );
   }
 }
