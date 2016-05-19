8d3ddfb7f8cf6af1904be6f127aab40f20391ff1
bedrich <me@bedrichrios.com>
2016-05-19 16:55:57 -0400

    Add SassMiddleware

--- server.py
+++ server.py
@@ -1,6 +1,9 @@
 from flask import Flask, render_template
+from sassutils.wsgi import SassMiddleware
 app = Flask(__name__, static_url_path="", static_folder="src")
-
+app.wsgi_app = SassMiddleware(app.wsgi_app, {
+    'src': ('scss', 'css', '/css')
+})
 
 @app.route("/")
 def index():
